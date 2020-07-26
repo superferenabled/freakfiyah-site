7f62812a44a6c9a3962e5627b9e852ae0250e304
Fernando Castillo <superferenabled@gmail.com>
2020-07-26 15:40:21 -0500

    added the styles for the tiles in the games and blog section

--- src/sections/Games/Games.tsx
+++ src/sections/Games/Games.tsx
@@ -7,17 +7,17 @@ const Games: React.FC = (props: any) => {
     const [games, setGames] = useState([
         {
             title: 'Curse of the Dark Tower',
-            text: 'bla blah',
+            text: 'We are very excited to announce that we will participate in the Indie Game Maker Contest 2015. The Curse of the Dark Tower is a game that we started on july 9 and that we finish today, just a day before the deadline, we are very happy with the obtained result. We invite you all to try out our game, you can download it for free in the link below. Wish us luck.',
             image: 'image.jpg'
         },
         {
             title: 'Curse of the Dark Tower',
-            text: 'bla blah',
+            text: 'We are very excited to announce that we will participate in the Indie Game Maker Contest 2015. The Curse of the Dark Tower is a game that we started on july 9 and that we finish today, just a day before the deadline, we are very happy with the obtained result. We invite you all to try out our game, you can download it for free in the link below. Wish us luck.',
             image: 'image.jpg'
         },
         {
             title: 'Curse of the Dark Tower',
-            text: 'bla blah',
+            text: 'We are very excited to announce that we will participate in the Indie Game Maker Contest 2015. The Curse of the Dark Tower is a game that we started on july 9 and that we finish today, just a day before the deadline, we are very happy with the obtained result. We invite you all to try out our game, you can download it for free in the link below. Wish us luck.',
             image: 'image.jpg'
         }
     ]);
