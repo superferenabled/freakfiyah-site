d92301a8a30a2a699d892938f9c421e55a58617b
Fernando Castillo <superferenabled@gmail.com>
2020-07-27 22:44:00 -0500

    added routing for the games section and added some images

--- src/components/tile/Tile.tsx
+++ src/components/tile/Tile.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
+import {
+    Link,
+  } from "react-router-dom";
 import classes from './Tile.module.scss';
 
 type Game = {
     title: string;
+    id: string,
     text: string;
+    shortDesc: string;
     image: string;
+    url: string;
 };
 
 const Tile: React.FC<Game> = (props) => {
     return (
+        <Link className={classes.TileLink} to={props.url}>
         <div className={classes.Tile}>
-            <span className={classes.Image}>{props.image}</span>
+            <span className={classes.Image}><img src={`./${props.image}`} alt={props.image}></img></span>
             <span className={classes.Info}>
                 <span className={classes.Title}>{props.title}</span>
-                <span className={classes.Text}>{props.text}</span>
+                <span className={classes.Text}>{props.shortDesc}</span>
             </span>
         </div>
+        </Link>
     );
 };
 
--- src/sections/Games/Games.tsx
+++ src/sections/Games/Games.tsx
@@ -1,33 +1,55 @@
 import React, {useState} from 'react';
 import classes from './Games.module.scss';
 import Tile from '../../components/tile/Tile';
+import {
+    Switch,
+    Route,
+    useRouteMatch,
+  } from "react-router-dom";
 
 const Games: React.FC = (props: any) => {
+    let match = useRouteMatch();
 
     const [games, setGames] = useState([
         {
-            title: 'Curse of the Dark Tower',
-            text: 'We are very excited to announce that we will participate in the Indie Game Maker Contest 2015. The Curse of the Dark Tower is a game that we started on july 9 and that we finish today, just a day before the deadline, we are very happy with the obtained result. We invite you all to try out our game, you can download it for free in the link below. Wish us luck.',
-            image: 'image.jpg'
+            title: 'Bombear',
+            id: 'bombear',
+            text: 'blah blah bombear stuff.',
+            shortDesc: 'We are very excited to announce that we will participate in the Indie Game Maker Contest 2015.',
+            image: 'bb.png'
+        },
+        {
+            title: 'Shape Escape',
+            id: 'shapeescape',
+            text: 'blah blah shape escape stuff.',
+            shortDesc: 'We are very excited to announce that we will participate in the Indie Game Maker Contest 2015.',
+            image: 'shpesc.png'
         },
         {
             title: 'Curse of the Dark Tower',
-            text: 'We are very excited to announce that we will participate in the Indie Game Maker Contest 2015. The Curse of the Dark Tower is a game that we started on july 9 and that we finish today, just a day before the deadline, we are very happy with the obtained result. We invite you all to try out our game, you can download it for free in the link below. Wish us luck.',
-            image: 'image.jpg'
-        },
-        {
-            title: 'Curse of the Dark Tower',
-            text: 'We are very excited to announce that we will participate in the Indie Game Maker Contest 2015. The Curse of the Dark Tower is a game that we started on july 9 and that we finish today, just a day before the deadline, we are very happy with the obtained result. We invite you all to try out our game, you can download it for free in the link below. Wish us luck.',
-            image: 'image.jpg'
+            id: 'curseofthedarktower',
+            text: 'blah blah curse of the dark tower stuff.',
+            shortDesc: 'We are very excited to announce that we will participate in the Indie Game Maker Contest 2015.',
+            image: 'cotdt.png'
         }
     ]);
 
     return (
         <div className={classes.Section}>
+        <Switch>
             {games.map(game => {
-                return <Tile {...game}></Tile>
+                return (
+                <Route path={`${match.path}/${game.id}`}>
+                    <div>{game.title}</div>
+                    <div>{game.text}</div>
+                </Route>
+                );
             })}
-            
+        </Switch>
+
+        {games.map(game => {
+            return <Tile {...game} url={`${match.path}/${game.id}`}></Tile>
+        })}
         </div>
     );
 };
